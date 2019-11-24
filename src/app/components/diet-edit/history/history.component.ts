a4423c17084a3cdd111f0986260d49efa820fcba
Denis Panov <deonis.panoff@gmail.com>
2019-11-24 11:40:26 +0100

    chart fix

--- src/app/components/diet-edit/history/history.component.ts
+++ src/app/components/diet-edit/history/history.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FireBaseService} from '../../../services/fire-base.service';
-import { ChartsModule } from 'ng2-charts';
-import { ChartDataSets, ChartOptions } from 'chart.js';
-import { Color, Label } from 'ng2-charts';
+import {ChartsModule} from 'ng2-charts';
+import {ChartDataSets, ChartOptions} from 'chart.js';
+import {Color, Label} from 'ng2-charts';
 
 @Component({
   selector: 'app-history',
@@ -13,11 +13,11 @@ export class HistoryComponent implements OnInit {
 
   private subs = [];
   historyList = [];
-  login = " "
+  login = ' ';
 
   public plots = [];
 
-  public lineChartData: ChartDataSets[] = [{"data": [], "label": ""}];
+  public lineChartData: ChartDataSets[] = [{'data': [], 'label': ''}];
   public lineChartLabels: Label[] = [];
   public lineChartOptions: (ChartOptions) = {
     responsive: true,
@@ -33,7 +33,8 @@ export class HistoryComponent implements OnInit {
   public lineChartPlugins = [];
 
 
-  constructor(private fireBase: FireBaseService) { }
+  constructor(private fireBase: FireBaseService) {
+  }
 
   ngOnInit() {
     this.loadData();
@@ -42,49 +43,49 @@ export class HistoryComponent implements OnInit {
   private loadData(): void {
     this.subs.push(
       this.fireBase.getHistoryList().subscribe(history => {
-        this.historyList = history;       
+        this.historyList = history;
         //this.historyList.push({"login":"abcd", "Weight":34, "date": new Date()})
         //this.historyList.push({"login":"abcd", "Weight":35, "date": new Date()})
         //this.historyList.push({"login":"abcd", "Weight":36, "date": new Date()})
-        var logins = (new Set(this.historyList.map(h => h.login)))
+        var logins = (new Set(this.historyList.map(h => h.login)));
         console.log(logins);
 
         logins.forEach(
           l => {
-            this.plots.push(this.getUserDataPlot(this.historyList.filter(h => h.login == l))) 
+            this.plots.push(this.getUserDataPlot(this.historyList.filter(h => h.login == l)));
           }
-        )
-        
+        );
+
         console.log(history);
       })
     );
   }
 
-  private getUserDataPlot(array){
+  private getUserDataPlot(array) {
     var plotData = {
-      "lineChartData": [{"data": [], "label": ""}],
-      "lineChartLabels": [],
-      "login": ""
+      'lineChartData': [{'data': [], 'label': ''}],
+      'lineChartLabels': [],
+      'login': ''
     };
     //sort by date
-    array = array.sort(function(a,b){
-      return (a.date.seconds * 1000) - (b.date.seconds * 1000)
+    array = array.sort(function(a, b) {
+      return (a.date.seconds * 1000) - (b.date.seconds * 1000);
     });
 
-    var weightList = []
-    var labelsList = []
+    var weightList = [];
+    var labelsList = [];
 
     array.forEach(l => weightList.push(l.Weight));
     array.forEach(l => labelsList.push(
       (new Date(l.date.seconds * 1000).getDate())
-      +"."+(new Date(l.date.seconds * 1000).getMonth())
-      +"."+(new Date(l.date.seconds * 1000).getFullYear())
-      ));
+      + '.' + (new Date(l.date.seconds * 1000).getMonth())
+      + '.' + (new Date(l.date.seconds * 1000).getFullYear())
+    ));
 
-      plotData.lineChartData = [
-      { data: weightList, label: array[0].login },
+    plotData.lineChartData = [
+      {data: weightList, label: array[0].login},
     ];
-    plotData.lineChartLabels = labelsList
+    plotData.lineChartLabels = labelsList;
     plotData.login = array[0].login;
 
     return plotData;
